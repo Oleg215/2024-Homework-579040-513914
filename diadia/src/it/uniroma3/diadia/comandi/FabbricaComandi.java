35cd4721b4dd60b38683508578e4c9e2bc8df0db
EricaGarg <gargerica@hotmail.com>
2024-06-10 20:32:33 +0200

    prova con aggiunta della classe FabbricaDiComandiRiflessiva commentata

--- diadia/src/it/uniroma3/diadia/comandi/FabbricaComandi.java
+++ diadia/src/it/uniroma3/diadia/comandi/FabbricaComandi.java
@@ -1,6 +1,6 @@
 package it.uniroma3.diadia.comandi;
 
 public interface FabbricaComandi {
-	public Command costruisciComando(String istruzione);
+	public Command costruisciComando(String istruzione) throws Exception;;
 
 }
